79c0f11ebaa4a3d82e6ddd75fec655b835320d6f
Nicholas Poole <nicholas.poole@everi.com>
2024-06-13 10:12:31 -0700

    Initial Commit

---
+++ cSharpDataStructures/Stack.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace cSharpDataStructures
+{
+    internal class Stack<T>(int size)
+    {
+        private List<T> _stack = new List<T>(size);
+
+        public List<T> MyStack { get; set; }
+    }
+}
