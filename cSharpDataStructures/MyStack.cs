0e1eb91173c85b9e195c0c5c3e12efa2982a5ac3
Nicholas Poole <nicholas.poole@everi.com>
2024-06-13 13:45:25 -0700

    Untested MyStack class

---
+++ cSharpDataStructures/MyStack.cs
@@ -0,0 +1,30 @@
+﻿
+namespace cSharpDataStructures
+{
+    internal class MyStack<T>
+    {
+        private List<T> _stack;
+
+        public List<T> Stack { get => _stack; set => _stack = value; }
+
+        public MyStack(int size)
+        {
+            Stack = new List<T>(size);
+        }
+
+        public T ReadStack()
+        {
+            return Stack[0];
+        }
+
+        public T Pop() 
+        { 
+            return Stack[Stack.Count - 1];
+        }
+
+        public void Push(T item)
+        {
+            Stack[Stack.Count - 1] = item;
+        }
+    }
+}
